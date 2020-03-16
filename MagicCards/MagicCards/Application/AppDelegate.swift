c9c59b13c54409dde769f7a874b63bffb805d6de
Adriel de Arruda Moura Freire <adriel.de.a.freire@concrete.com.br>
2020-03-16 16:41:39 -0300

    Add CoreData test container

--- MagicCards/MagicCards/Application/AppDelegate.swift
+++ MagicCards/MagicCards/Application/AppDelegate.swift
@@ -14,9 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = TestController()
-        window?.makeKeyAndVisible()
         return true
     }
 
