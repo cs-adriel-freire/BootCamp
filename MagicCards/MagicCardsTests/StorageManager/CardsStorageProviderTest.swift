7be078035532d3e62a2bf3d3e7d52728139f3826
Adriel de Arruda Moura Freire <adriel.de.a.freire@concrete.com.br>
2020-03-16 23:35:17 -0300

    Add CardStorageProviderTest

---
+++ MagicCards/MagicCardsTests/StorageManager/CardsStorageProviderTest.swift
@@ -0,0 +1,20 @@
+//
+//  CardsStorageProviderTest.swift
+//  MagicCardsTests
+//
+//  Created by Adriel de Arruda Moura Freire on 16/03/20.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import XCTest
+@testable import MagicCards
+
+class CardsStorageProvideTest: XCTestCase {
+    override class func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
