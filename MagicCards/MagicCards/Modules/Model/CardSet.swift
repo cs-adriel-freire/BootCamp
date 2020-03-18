3ed3e292ab10ba906dae860208b638ea5b203f63
Lucca França <lucca.f.ferreira@concrete.com.br>
2020-03-18 17:33:36 -0300

    Add model classes initializers

--- MagicCards/MagicCards/Modules/Model/CardSet.swift
+++ MagicCards/MagicCards/Modules/Model/CardSet.swift
@@ -10,6 +10,8 @@ import Foundation
 
 final class CardSet: Codable {
     
+    // MARK: - Variables
+    
     let id: String
     let name: String
     let releaseDate: Date
@@ -21,6 +23,18 @@ final class CardSet: Codable {
         decoder.dateDecodingStrategy = .formatted(dateFormatter)
         return decoder
     }
+    
+    // MARK: - Methods
+
+    // MARK: Initializers
+    
+    init(id: String, name: String, releaseDate: Date) {
+        self.id = id
+        self.name = name
+        self.releaseDate = releaseDate
+    }
+    
+    // MARK: - Enums
     
     private enum CodingKeys: String, CodingKey {
         case id = "code"
