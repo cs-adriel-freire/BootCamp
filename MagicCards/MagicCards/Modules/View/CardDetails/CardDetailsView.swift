0d9547f76932e68dfe361373934dc29a27faa32c
Adriel de Arruda Moura Freire <adriel.de.a.freire@concrete.com.br>
2020-03-18 15:31:17 -0300

    Add constraints

---
+++ MagicCards/MagicCards/Modules/View/CardDetails/CardDetailsView.swift
@@ -0,0 +1,130 @@
+//
+//  CardDetailsView.swift
+//  MagicCards
+//
+//  Created by Adriel de Arruda Moura Freire on 18/03/20.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+final class CardDetailsView: UIView {
+    
+    // MARK: - Properties
+    public weak var delegate: CardDetailsViewDelegate?
+    
+    private let cards: [Card]
+    
+    private let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(named: "backgroundImage")
+        imageView.image = image
+        return imageView
+    }()
+    
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "closeIcon"), for: .normal)
+        return button
+    }()
+    
+    private let addFavoriteButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("add card to deck", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        
+        button.backgroundColor = .clear
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        
+        return button
+    }()
+    
+    private let cardsCollection: UICollectionView = {
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        return collection
+    }()
+    
+    // MARK: - Initializers
+    
+    convenience init(cards: [Card]) {
+        self.init(frame: .zero, cards: cards)
+    }
+    
+    init(frame: CGRect, cards: [Card]) {
+        self.cards = cards
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+}
+
+extension CardDetailsView: ViewCode {
+    // MARK: - ViewCode
+    
+    func buildViewHierarchy() {
+        addSubview(backgroundImageView)
+        addSubview(cardsCollection)
+        addSubview(closeButton)
+        addSubview(addFavoriteButton)
+    }
+    
+    func setupContraints() {
+        backgroundImageView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+        addFavoriteButton.snp.makeConstraints { (make) in
+            make.height.equalTo(48)
+            
+            if #available(iOS 11.0, *) {
+                make.bottom.equalTo(safeAreaLayoutGuide).offset(-16)
+                make.leading.equalTo(safeAreaLayoutGuide).offset(16)
+                make.trailing.equalTo(safeAreaLayoutGuide).offset(-16)
+            } else {
+                make.leading.equalToSuperview().offset(16)
+                make.trailing.equalToSuperview().offset(-16)
+                make.bottom.equalToSuperview().offset(-16)
+            }
+        }
+        
+        closeButton.snp.makeConstraints { (make) in
+            make.width.equalTo(44)
+            make.height.equalTo(44)
+            
+            if #available(iOS 11.0, *) {
+                make.top.equalTo(safeAreaLayoutGuide)
+                make.leading.equalTo(safeAreaLayoutGuide).offset(8)
+            } else {
+                make.leading.equalToSuperview().offset(8)
+                make.top.equalToSuperview()
+            }
+        }
+        
+        cardsCollection.snp.makeConstraints { (make) in
+            make.height.equalTo(265)
+            make.center.equalToSuperview()
+            
+            if #available(iOS 11.0, *) {
+                make.leading.equalTo(safeAreaLayoutGuide)
+                make.trailing.equalTo(safeAreaLayoutGuide)
+            } else {
+                make.leading.equalToSuperview()
+                make.trailing.equalToSuperview()
+            }
+        }
+    }
+       
+    func setupAdditionalConfiguration() {
+        
+    }
+}
---
+++ MagicCards/MagicCards/Modules/View/CardDetails/CardsDetailViewDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CardsDetailViewDelegate.swift
+//  MagicCards
+//
+//  Created by Adriel de Arruda Moura Freire on 18/03/20.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import Foundation
+protocol CardDetailsViewDelegate: AnyObject {
+    
+}
