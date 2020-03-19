3bbbcfc5f5ac819c5e88b2960fe85817e0328ed9
Lucca França <lucca.f.ferreira@concrete.com.br>
2020-03-19 11:50:29 -0300

    Add FavoritesViewController

--- MagicCards/MagicCards/Modules/View/Controllers/FavoritesViewController.swift
+++ MagicCards/MagicCards/Modules/View/Controllers/FavoritesViewController.swift
@@ -10,4 +10,60 @@ import UIKit
 
 final class FavoritesViewController: UIViewController {
     
+    // MARK: - Variables
+    
+    // MARK: View
+     private lazy var gridView = FavoritesCardsGridView(viewModel: self.viewModel, collectionDelegate: self)
+
+    // MARK: ViewModel
+    private var viewModel: CardsFavoritesGridViewModel {
+        didSet {
+            self.gridView.viewModel = self.viewModel
+        }
+    }
+
+    // MARK: Data
+//    let cardsRepository: FavoriteCardsRepositoryProtocol
+
+    // MARK: - Methods
+    // MARK: Initializers
+    // init(repository: FavoriteCardsRepositoryProtocol) {
+    //    self.cardsRepository = repository
+    //    super.init(nibName: nil, bundle: nil)
+    // }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Life cycle
+    override func loadView() {
+        self.view = self.gridView
+    }
+
+    override func viewDidLayoutSubviews() {
+        self.gridView.updateFrame()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    // MARK: Update data
+    private func getMoreCards() {
+        // let favoriteCards = self.cardsRepository.getFavoriteCards()
+        // let viewModel = CardsFavoritesGridViewModel(cards: favoriteCards)
+        
+    }
+    
+}
+
+// MARK: - UICollectionViewDelegate
+extension FavoritesViewController: UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath == IndexPath(item: self.viewModel.numberOfItems - 1, section: 0) {
+            self.getMoreCards()
+        }
+    }
 }
---
+++ MagicCards/MagicCards/Modules/View/FavoriteCardsGridView.swift
@@ -0,0 +1,86 @@
+//
+//  FavoriteCardsGridView.swift
+//  MagicCards
+//
+//  Created by lucca.f.ferreira on 19/03/20.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import UIKit
+
+final class FavoritesCardsGridView: UIView {
+    
+    // MARK: - Variables
+
+    var viewModel: CardsFavoritesGridViewModel {
+        didSet {
+            self.gridFavoritesCollectionDataSource.viewModel = self.viewModel
+            self.collectionView.reloadData()
+        }
+    }
+
+    // MARK: CollectionVIew
+
+    var collectionFlowLayout: CardsGridViewFlowLayout
+    private let gridFavoritesCollectionDataSource: GridFavoritesCollectionDataSource
+    //swiftlint:disable weak_delegate
+    private let collectionViewDelegate: UICollectionViewDelegate?
+    //swiftlint:enable weak_delegate
+
+    // MARK: Subviews
+
+    private lazy var backgroundImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "background")
+        return view
+    }()
+
+    private lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: self.collectionFlowLayout)
+        view.register(CardCell.self, forCellWithReuseIdentifier: CardCell.reuseIdentifier)
+        view.dataSource = self.gridFavoritesCollectionDataSource
+        view.delegate = self.collectionViewDelegate
+        view.backgroundColor = .clear
+        return view
+    }()
+
+    // MARK: - Methods
+
+    // MARK: Initializers
+
+    init(frame: CGRect = .zero, viewModel: CardsFavoritesGridViewModel, collectionDelegate: UICollectionViewDelegate? = nil) {
+        self.collectionViewDelegate = collectionDelegate
+        self.viewModel = viewModel
+        self.collectionFlowLayout = CardsGridViewFlowLayout()
+        self.gridFavoritesCollectionDataSource = GridFavoritesCollectionDataSource(viewModel: self.viewModel)
+        super.init(frame: frame)
+
+        self.setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension FavoritesCardsGridView: ViewCode {
+
+    func buildViewHierarchy() {
+        self.addSubview(self.backgroundImageView)
+        self.backgroundImageView.addSubview(self.collectionView)
+    }
+
+    func setupContraints() {
+        self.backgroundImageView.snp.makeConstraints { maker in
+            maker.top.bottom.leading.trailing.equalToSuperview()
+        }
+
+        self.collectionView.snp.makeConstraints { maker in
+            maker.top.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+
+    func setupAdditionalConfiguration() {
+        self.collectionFlowLayout.collectionFrame = self.frame
+    }
+}
