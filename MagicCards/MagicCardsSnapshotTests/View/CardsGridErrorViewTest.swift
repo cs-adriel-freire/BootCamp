dd42f02ed9b9f75906983661ffbd436c492ea393
Lucca França <lucca.f.ferreira@concrete.com.br>
2020-03-20 18:49:58 -0300

    Add refreshImage files

---
+++ MagicCards/MagicCardsSnapshotTests/View/CardsGridErrorViewTest.swift
@@ -0,0 +1,20 @@
+//
+//  CardGridErrorViewTest.swift
+//  MagicCardsSnapshotTests
+//
+//  Created by lucca.f.ferreira on 20/03/20.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import Nimble
+import Nimble_Snapshots
+import XCTest
+
+@testable import MagicCards
+
+final class CardsGridErrorViewTest: XCTestCase {
+    
+}
