692d23a7a1a77ed829317c8947c2f847f68c084f
Lucca França <lucca.f.ferreira@concrete.com.br>
2020-03-20 17:27:34 -0300

    Add subviews to CardsGridErrorView

--- MagicCards/MagicCards/Modules/View/CardsGridErrorView.swift
+++ MagicCards/MagicCards/Modules/View/CardsGridErrorView.swift
@@ -10,4 +10,57 @@ import UIKit
 
 final class CardsGridErrorView: UIView {
 
+    // MARK: - Variables
+    
+    // MARK: Subviews
+    
+    private let wrapperView: UIView = {
+        let view = UIView(frame: .zero)
+        return view
+    }()
+    
+    private let mainText: UILabel = {
+        let view = UILabel(frame: .zero)
+        view.font = UIFont.systemFont(ofSize: 12.0, weight: .semibold)
+        view.textAlignment = .center
+        view.text = "Sorry, something went wrong"
+        return view
+    }()
+    
+    private let secondaryText: UILabel = {
+        let view = UILabel(frame: .zero)
+        view.font = UIFont.systemFont(ofSize: 10.0, weight: .regular)
+        view.textAlignment = .center
+        view.text = "Press the buttom bellow to try again"
+        return view
+    }()
+    
+    private let retryButton: UIButton = {
+        let view = UIButton(frame: .zero)
+        // view.setImage(nil, for: .normal)
+        return view
+    }()
+    
+}
+
+
+// MARK: - ViewCode
+
+extension CardsGridErrorView: ViewCode {
+    
+    func buildViewHierarchy() {
+        self.addSubview(wrapperView)
+        self.wrapperView.addSubview(mainText)
+        self.wrapperView.addSubview(secondaryText)
+        self.wrapperView.addSubview(retryButton)
+    }
+    
+    func setupContraints() {
+        
+    }
+    
+    func setupAdditionalConfiguration() {
+    
+    }
+    
 }
