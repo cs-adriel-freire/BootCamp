5c0ec077ec527b949e0f5f5aceea378e4713fd82
Lucca França <lucca.f.ferreira@concrete.com.br>
2020-03-20 17:05:13 -0300

    Add CardsGridErrorView file

---
+++ MagicCards/MagicCards/Modules/View/CardsGridErrorView.swift
@@ -0,0 +1,13 @@
+//
+//  CardsGridErrorView.swift
+//  MagicCards
+//
+//  Created by lucca.f.ferreira on 20/03/20.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import UIKit
+
+final class CardsGridErrorView: UIView {
+
+}
